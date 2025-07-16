75ee0f0c9485ec2277a23dc38044af0c226180a9
Ella Åkesson Hummerdal <ella.belle.hummerdahl@gmail.com>
2025-07-16 21:29:34 +0200

    [Added] a tiny bit of starter code.

--- InlAug2025.py
+++ InlAug2025.py
@@ -6,3 +6,24 @@ filnamn ='gameresults.csv'
 
 # Skriv din kod här:
 
+import random
+
+
+
+
+# Introduktion till spelet
+print("Välkommen till Templets Tio Dörrar!")
+print("Enligt sägnen finns här en enorm skatt gömd, men ingen vet exakt var. Templet består av ett hemligt kammarsystem med tio massiva dörrar – och bakom endast en av dem vilar skatten.")
+print("Men här är haken: För varje fel dörr du öppnar, halveras skatten.")
+print("Skattens värde krymper alltså för varje gång du chansar fel.")
+print("Välj noga och lycka till!\n")
+
+# Startmeny
+print("SPELETS STARTMENY")
+print("1. Spela Skattjakten")
+print("2. Visa topplistan")
+print("3. Visa ett diagram över tidigare resultat")
+print("4. Avsluta spelet")
+
+val = input("Välj ett alternativ (1-3): ")
+
