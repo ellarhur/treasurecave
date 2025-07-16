94d906b16379ff4b371c369010442c3b2963f254
Ella Åkesson Hummerdal <ella.belle.hummerdahl@gmail.com>
2025-07-16 21:19:29 +0200

    Första commit – lade till gameresults

---
+++ InlAug2025.py
@@ -0,0 +1,8 @@
+# Svarsfil till inlämningsuppgiften i Grundläggande programmering i Python augusti
+2025.
+
+# Denna rad används för att namnge CSV-filen i koden. Använd sedan variabeln "filnamn" när du skapar din kod.
+filnamn ='gameresults.csv'
+
+# Skriv din kod här:
+
